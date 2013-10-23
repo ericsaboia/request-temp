746065532dadcccb224e4a6947e60ab2a7e50314
Ricardo Tomasi <ricardobeat@gmail.com>
2013-10-23 14:47:10 -0200

    simplify code, return method instead of request module

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-var path    = require('path')
-  , url     = require('url')
-  , os      = require('os')
-  , fs      = require('fs')
+var path = require('path')
+  , url  = require('url')
+  , os   = require('os')
+  , fs   = require('fs')
   , request
-
-function initParams (uri, options, callback) {
-    if ((typeof options === 'function') && !callback){
-        callback = options
-    }
-    if (options && typeof options === 'object') {
-        options.uri = uri
-    } else if (typeof uri === 'string') {
-        options = {uri:uri}
-    } else {
-        options = uri
-        uri = options.uri
-    }
-    return { uri: uri, options: options, callback: callback }
-}
 
 function tempFile (_url) {
     var name = path.basename(url.parse(_url).pathname) || Math.floor(Math.random() * 1e20).toString(16)
@@ -29,25 +14,23 @@ function tempFile (_url) {
     }
 }
 
-function tempRequest (_url, options, callback) {
-    var params   = initParams(_url, options, callback)
-      , options  = params.options
-      , callback = params.callback
-      , file     = tempFile(options.uri || options.url)
-      , tmp      = fs.createWriteStream(file.path)
+function tempRequest (_url) {
+    var args = Array.prototype.slice.call(arguments, 0)
+      , _url = (typeof _url === 'string') ? _url : _url.uri || _url.url
+      , file = tempFile(_url)
+      , cb   = args[args.length - 1]
+      , tmp  = fs.createWriteStream(file.path)
 
-    function done (err, res, body) {
-        if (res.statusCode >= 300) {
-            return callback(new Error("Request failed (" + res.statusCode + ")"))
+    if (typeof cb === 'function') {
+        args[args.length - 1] = function (err, res, body) {
+            cb(err, res, body, file)
         }
-        callback(err, res, body, file)
     }
 
-    return request.get(options, done).pipe(tmp)
+    return request.get.apply(request, args).pipe(tmp)
 }
 
 module.exports = function (_request) {
     request = _request
-    request.temp = tempRequest
-    return request
+    return request.temp = tempRequest
 }
